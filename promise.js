40e2452e857eb239c17218994734ba9aca422aa6
windi <windi.azmy@gmail.com>
2022-06-15 09:27:20 +0700

    complete task

--- promise.js
+++ promise.js
@@ -1,7 +1,38 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const jmlhData = (data, indexData) => {
+
+    return new Promise((resolve, reject) => {
+      if(data === null || data === "" ){
+        reject("data not found!")
+      }else {
+        let result = 0;
+         for(let i = 0; i < data.length; i++){
+           if(indexData === data[i]['hasil']){
+             result++;
+           }
+         }
+         resolve(result);
+      }
+    })
+}
+const promiseOutput = async(indexData) => {
+          
+       
+      const theaterIXX = await promiseTheaterIXX()
+        .then((dataHasil) => jmlhData(dataHasil, indexData));
+
+
+      const theaterVGC = await promiseTheaterVGC()
+        .then((dataHasil) => jmlhData(dataHasil, indexData));
+
+
+      const final =  theaterVGC + theaterIXX;
+      return final;
+    
+}
 
 module.exports = {
   promiseOutput,
